c7df09e95f52822c0915b5ee2de5fb895451dab9
Maxime Daniel <root@maxux.net>
2016-07-01 20:26:31 +0200

    updating readme and makefile

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # sms-gateway
 
 SMS Gateway via 3G-Modem dongle
+
+# Compiling
+`cd src && make`
+
+# Usage
+./src/sms-gateway
--- src/config.mk
+++ src/config.mk
@@ -1,6 +1,6 @@
-EXEC = serialinfo
+EXEC = sms-gateway
 
-VERSION = 0.1
+VERSION = 0.2
 CFLAGS  = -W -Wall -O2 -pipe -ansi -std=gnu99 -pthread -g
 LDFLAGS = -lsqlite3 -lm
 
